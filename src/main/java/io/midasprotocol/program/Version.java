ab5b96e6311833935df6fa2ac07ac55f0a2a225b
Nghia Ngo <ngonghiahp93@gmail.com>
2019-06-25 14:25:36 +0700

    Update version

--- src/main/java/io/midasprotocol/program/Version.java
+++ src/main/java/io/midasprotocol/program/Version.java
@@ -1,9 +1,9 @@
 package io.midasprotocol.program;
 
 public class Version {
-	public static final String versionName = "Mcash-v06.06";
 	public static final String versionCode = "1";
 	private static final String version = "0.1.0";
+	public static final String versionName = "Mcashchain-v" + version;
 
 	public static String getVersion() {
 		return version;
